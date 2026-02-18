63e4d9ff76fc9f815804d6e1019a61d00b586472
Caelin <caelouwho@gmail.com>
2026-02-18 01:35:32 -0500

    fix loader on pages with no song

--- public/js/preloader.js
+++ public/js/preloader.js
@@ -1,26 +1,38 @@
-
 var loader = document.getElementById("preloader");
-let loadedDocument = false;
-let loadedSong = false
 
 if (window.location.hostname === "localhost") {
   loader.style.opacity = 0;
   loader.style.pointerEvents = "none";
 }
-window.addEventListener("load", function() {
-  loadedDocument = true;
 
-  if (loadedDocument && loadedSong) {
+
+if (loader.dataset.waitSong != undefined) {
+  console.log("WAIT SONG")
+
+  let loadedDocument = false;
+  let loadedSong = false
+
+  window.addEventListener("load", function() {
+    loadedDocument = true;
+
+    if (loadedDocument && loadedSong) {
+      loader.style.opacity = 0;
+      loader.style.pointerEvents = "none";
+    }
+  });
+
+  window.addEventListener("song-load", function() {
+    loadedSong = true;
+
+    if (loadedDocument && loadedSong) {
+      loader.style.opacity = 0;
+      loader.style.pointerEvents = "none";
+    }
+  });
+} else {
+  window.addEventListener("load", function() {
     loader.style.opacity = 0;
     loader.style.pointerEvents = "none";
-  }
-});
+  });
+}
 
-window.addEventListener("song-load", function() {
-  loadedSong = true;
-
-  if (loadedDocument && loadedSong) {
-    loader.style.opacity = 0;
-    loader.style.pointerEvents = "none";
-  }
-});
